ac5b13cfd8205da8601e9ca6bf79a734b48ac515
Albert Vilella <avilella@gmail.com>
2011-05-25 14:12:01 +0100

    cleaned up and added detailed instructions at the bottom

--- scripts/ensembl_snp_gwas_epo.pl
+++ scripts/ensembl_snp_gwas_epo.pl
@@ -24,7 +24,6 @@ Bio::EnsEMBL::Registry->no_version_check(1) unless ($debug);
 
 # variation
 my $var_adaptor = Bio::EnsEMBL::Registry->get_adaptor($query,"variation","variation");
-my $pop_adaptor = Bio::EnsEMBL::Registry->get_adaptor($query,"variation","population");
 
 # compara
 my $comparaDBA = Bio::EnsEMBL::Registry->get_DBAdaptor('compara', 'compara');
@@ -116,10 +115,10 @@ while (<FILE>) {
         my $query_strand = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{strand};
         my $query_start           = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{start};
         my $query_end             = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{end};
-        my $target_seq_region_name = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{seq_region_name};
-        my $target_strand = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{strand};
-        my $target_start = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{start};
-        my $target_end = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{end};
+        my $target_seq_region_name = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{seq_region_name} || 'na';
+        my $target_strand = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{strand} || 'na';
+        my $target_start = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{start} || 'na';
+        my $target_end = $this_rsid->{rsid}{$rsid}{vfid}{$vfid}{target}{$target}{end} || 'na';
         $DB::single=$debug;1;
         print "$rsid\t$phenotype\t$associated_gene\t$source\t$external_ref\t$vfid\t$query\t$query_seq_region_name\t$query_strand\t$query_start\t$query_end\t$target\t$target_seq_region_name\t$target_strand\t$target_start\t$target_end\n";
       }
@@ -156,3 +155,61 @@ close FILE;
 # centred on the human GWAS hit and get their orthologous positions in
 # rat.
 
+# STEPS:
+
+# Install the Ensembl APIs:
+# http://www.ensembl.org/info/docs/api/api_installation.html
+
+# briefly: install the bioperl-1.2.3 code, ensembl, ensembl-variation and ensembl-compara API code (requires mysql client and DBD::mysql):
+
+# mkdir -p ~/eutebi/ensembl_main
+# cd ~/eutebi/ensembl_main
+# cvs -d :pserver:cvsuser@cvs.sanger.ac.uk:/cvsroot/ensembl checkout -r branch-ensembl-62 ensembl
+# cvs -d :pserver:cvsuser@cvs.sanger.ac.uk:/cvsroot/ensembl checkout -r branch-ensembl-62 ensembl-compara
+# cvs -d :pserver:cvsuser@cvs.sanger.ac.uk:/cvsroot/ensembl checkout -r branch-ensembl-62 ensembl-variation
+# cd ~/eutebi
+# wget http://bioperl.org/DIST/old_releases/bioperl-1.2.3.tar.gz -O /tmp/bioperl-1.2.3.tar.gz
+# tar xzf /tmp/bioperl-1.2.3.tar.gz
+
+# Put it in your path:
+# export PERL5LIB=$HOME/eutebi/ensembl_main/ensembl/modules:$HOME/eutebi/ensembl_main/ensembl-hive/modules:$HOME/eutebi/ensembl_main/ensembl-variation/modules:$HOME/eutebi/ensembl_main/ensembl-compara/modules
+
+# Check that the ensembl, ensembl-variation and ensembl-compara modules are visible:
+# perldoc Bio::EnsEMBL::Variation::Study
+# perldoc Bio::EnsEMBL::Gene
+# perldoc Bio::EnsEMBL::Compara::GenomeDB
+
+# Find the list of gwas snps (here for human v62):
+
+# mysql -hensembldb.ensembl.org -P5306 -uanonymous homo_sapiens_variation_62_37g -e "SELECT   v.name AS variation,   p.description AS phenotype,   va.associated_gene,   s.name AS source,   st.external_reference FROM   variation_annotation va JOIN   variation v ON (v.variation_id = va.variation_id) JOIN   phenotype p ON (p.phenotype_id = va.phenotype_id) JOIN   study st ON (st.study_id = va.study_id) JOIN   source s ON (s.source_id = st.source_id) limit 10"
+# +------------+-----------+-----------------+--------------------+--------------------+
+# | variation  | phenotype | associated_gene | source             | external_reference |
+# +------------+-----------+-----------------+--------------------+--------------------+
+# | rs11206801 | AB1-42    | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs2075650  | AB1-42    | TOMM40          | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs239713   | AB1-42    | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs1727638  | AB1-42    | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs12534221 | AB1-42    | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs10784496 | AB1-42    | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs2899472  | AB1-42    | CYP19A1         | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs7631605  | P-tau181p | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs7558386  | P-tau181p | Intergenic      | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# | rs12643654 | P-tau181p | UNC5C           | NHGRI_GWAS_catalog | pubmed/20932310    | 
+# +------------+-----------+-----------------+--------------------+--------------------+
+
+# Then save it to a file, "> input.tsv", and query the file with this script:
+# perl $HOME/eutebi/ensembl_snp_gwas_epo.pl -i input.tsv -target rattus_norvegicus -flanks 2500
+
+# It will produce an extended tabular output like this:
+
+# rsid        phenotype  associated_gene  source              external_ref     vfid      query         query_seq_region_name  query_strand  query_start  query_end  target             target_seq_region_name  target_strand  target_start  target_end
+# rs11206801  AB1-42     Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  8906441   homo_sapiens  1                      1             56848186     56853186   rattus_norvegicus  5                       -1             126299643     126303992
+# rs2075650   AB1-42     TOMM40           NHGRI_GWAS_catalog  pubmed/20932310  31031874  homo_sapiens  19                     1             45393119     45398119   rattus_norvegicus  1                       -1             79014495      79021635
+# rs239713    AB1-42     Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  1564067   homo_sapiens  21                     1             28693976     28698976   rattus_norvegicus  na                      na             na            na
+# rs1727638   AB1-42     Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  14142279  homo_sapiens  6                      1             72137072     72142072   rattus_norvegicus  9                       -1             22106476      22116695
+# rs12534221  AB1-42     Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  2523508   homo_sapiens  7                      1             131285490    131290490  rattus_norvegicus  4                       1              58692324      58695414
+# rs10784496  AB1-42     Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  21541316  homo_sapiens  12                     1             66158471     66163471   rattus_norvegicus  7                       -1             59810525      59814266
+# rs2899472   AB1-42     CYP19A1          NHGRI_GWAS_catalog  pubmed/20932310  23872662  homo_sapiens  15                     1             51513555     51518555   rattus_norvegicus  8                       1              57661176      57664187
+# rs7631605   P-tau181p  Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  17769149  homo_sapiens  3                      1             37232089     37237089   rattus_norvegicus  na                      na             na            na
+# rs7558386   P-tau181p  Intergenic       NHGRI_GWAS_catalog  pubmed/20932310  4614730   homo_sapiens  2                      1             227559639    227564639  rattus_norvegicus  9                       1              81552030      81559132
+# rs12643654  P-tau181p  UNC5C            NHGRI_GWAS_catalog  pubmed/20932310  27696656  homo_sapiens  4                      1             96157317     96162317   rattus_norvegicus  2                       -1             239657177     239662228
